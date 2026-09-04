1d48a4f9328493336d59d264c21f80e3916f455e
deguzmanadrian7272-alt <deguzmanadrian7272@gmail.com>
2026-09-04 17:08:26 +0800

    Update chapter1.js

--- js/chapter1.js
+++ js/chapter1.js
@@ -353,7 +353,7 @@ function startChapter1Intro() {
 
                     setTimeout(
                         revealChapter1Book,
-                        5000
+                        6000
                     );
 
                 }
@@ -376,7 +376,7 @@ function startChapter1Intro() {
 
     setTimeout(
         revealChapter1Book,
-        5000
+        6000
     );
 
 }
